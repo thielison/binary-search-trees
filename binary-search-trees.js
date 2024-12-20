b209c2251eddb0bab663ec091d59a6af7a5c5ca1
Thielison <67246254+thielison@users.noreply.github.com>
2024-12-20 18:42:03 -0300

    Add insert function to insert new nodes in the BST

--- binary-search-trees.js
+++ binary-search-trees.js
@@ -28,6 +28,26 @@ const tree = (arr, start, end) => {
     return root;
 };
 
+// Utility function to insert a new node with the given value
+const insert = (root, value) => {
+    if (root === null) {
+        return new Node(value);
+    }
+
+    // Duplicates not allowed
+    if (root.data === value) {
+        return root;
+    }
+
+    if (value < root.data) {
+        root.left = insert(root.left, value);
+    } else if (value > root.data) {
+        root.right = insert(root.right, value);
+    }
+
+    return root;
+};
+
 const buildTree = (arr) => {
     // Sort the input array and remove duplicates
     const uniqueSortedArray = sortWithoutDuplicates(arr);
@@ -38,4 +58,4 @@ const buildTree = (arr) => {
     return balancedSearchTree;
 };
 
-export { buildTree };
+export { buildTree, insert };
--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import { buildTree } from "./binary-search-trees.js";
+import { buildTree, insert } from "./binary-search-trees.js";
 import prettyPrintTree from "./pretty-print-tree.js";
 
 // Test array
@@ -6,6 +6,12 @@ const arr = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
 
 // Turns the test array in a balanced binary tree (BST)
 const balancedSearchTree = buildTree(arr);
+
+// Insert values in the balanced binary tree
+insert(balancedSearchTree, 2);
+insert(balancedSearchTree, 6);
+insert(balancedSearchTree, 10);
+insert(balancedSearchTree, 1234);
 
 // Print the BST
 prettyPrintTree(balancedSearchTree);
